138bc499c74bcc9f4ffe7d5c3279635b5ba1bd76
kalimuthu949 <kalimuthu210@gmail.com>
2023-01-26 14:30:47 +0530

    first push

---
+++ src/webparts/departPivot/components/DepartPivot.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import styles from './DepartPivot.module.scss';
+import { IDepartPivotProps } from './IDepartPivotProps';
+import { escape } from '@microsoft/sp-lodash-subset';
+import { sp } from "@pnp/sp";
+import { graph } from "@pnp/graph";
+import NewPivot from './NewPivot';
+export default class DepartPivot extends React.Component<IDepartPivotProps, {}> {
+  constructor(prop: IDepartPivotProps, state: {}) {
+    super(prop);
+    sp.setup({
+      spfxContext: this.props.context,
+    });
+    graph.setup({
+      spfxContext: this.props.context,
+    });
+  }
+  public render(): React.ReactElement<IDepartPivotProps> {
+    const {
+      description,
+      isDarkTheme,
+      environmentMessage,
+      hasTeamsContext,
+      userDisplayName
+    } = this.props;
+
+    return (
+    <NewPivot/>
+    );
+  }
+}
---
+++ src/webparts/departPivot/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IDepartPivotWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'DepartPivotWebPartStrings' {
+  const strings: IDepartPivotWebPartStrings;
+  export = strings;
+}
